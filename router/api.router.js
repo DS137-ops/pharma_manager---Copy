a70bb3403abaa32077ab387acab141ed5d1189f0
fayad <feadkaffoura@gmail.com>
2024-11-23 17:40:10 +0200

    nn

--- router/api.router.js
+++ router/api.router.js
@@ -2,7 +2,7 @@ const authmodel = require('../model/auth.model')
 const router = require('express').Router()
 const body = require('express').urlencoded({ extended: true })
 
-router.get('/',(req,res)=>{
+router.post('/',body,(req,res)=>{
     authmodel.getRegisterPageForApi(req.body.name, req.body.email , req.body.password , req.body.age , req.body.address , req.body.job).then(()=>{
         res.json({error:false , message:'success'})
     }).catch((err)=>{
