26402e76217034d459999863668d2c0ed4ea556b
fayad <feadkaffoura@gmail.com>
2025-04-11 00:06:28 +0300

    server error

--- controllers/analyst.controller.js
+++ controllers/analyst.controller.js
@@ -399,7 +399,7 @@ exports.updateAnalystInfo = async (req, res) => {
       const errors = Object.values(err.errors).map((e) => e.message);
       return res.status(400).json({ success: false, message: errors.join(', ') });
     }
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ success: false, message: `Internal server error ${err} `});
   }
 };
 
