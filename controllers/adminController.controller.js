f5879ba653b889f0e4d4fa2326a280a585311481
fayad <feadkaffoura@gmail.com>
2025-04-29 00:17:20 +0300

    add admin

--- controllers/adminController.controller.js
+++ controllers/adminController.controller.js
@@ -1,6 +1,7 @@
 const Admin = require("../model/adminModel.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const RefreshToken = require('../model/RefreshToken.model');
 
 exports.createAdmin = async (req, res) => {
   try {
@@ -9,9 +10,11 @@ exports.createAdmin = async (req, res) => {
 
     const { username, password } = req.body;
     const newAdmin = new Admin({ username, password });
+     const token = await jwt.sign({ role: 'admin' }, process.env.JWT_SECRET);
     await newAdmin.save();
+        await RefreshToken.create({ token, userRef: newAdmin._id });
     
-    res.status(200).json({ message: "Admin created successfully" });
+    res.status(200).json({ message: "Admin created successfully" , token });
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
@@ -21,12 +24,16 @@ exports.adminLogin = async (req, res) => {
   try {
     const { username, password } = req.body;
     const admin = await Admin.findOne({ username });
+    if(!admin)return res.status(404).json({message:'no admin'})
+    await RefreshToken.deleteMany({ userRef: admin._id });
 
     if (!admin || !(await bcrypt.compare(password, admin.password))) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: admin._id }, '1001110');
+  const token = jwt.sign({ id: admin._id, role: 'admin' }, '1001110');
+
+    await RefreshToken.create({ token, userRef: admin._id });    
     res.json({ message: "Login successful", token });
   } catch (error) {
     res.status(500).json({ error: `Internal server error ${error}` });
