4383a6c94b7fe154e2313f1c7f49709e0e3a1968
fayad <feadkaffoura@gmail.com>
2025-05-14 22:53:48 +0300

    update password

--- model/seek.model.js
+++ model/seek.model.js
@@ -1,63 +1,65 @@
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
-var mongoose = require('mongoose')
-    const SeekSchema = new mongoose.Schema({
-        fullName:{
-            type:String,
-            required:true,
-            trim:true
-        },
-        phone:{
-            type:String,
-            required:true,
-            match: [/^(\+20|0)1[0-9]{9}$/, "Please enter a valid Egyptian phone number"]
-        },
-        password:{
-            type:String,
-            required: [true, 'Password is required'],
-            minlength: [8, 'Password must be at least 8 characters long']
-        },
-        resetCode: { type: String, default: null },
-        resetCodeExpires: { type: Date, default: null },
-        role:{
-            type:String,
-            default:"user",
-            enum:["user"]
-        },
-        age:{
-          type:Number,
-          default:null
-        },
-        city:{
-            type:String,
-            default:null
-        },
-        region:{
-            type:String,
-            default:null
-        },
-        accountDate: {
-            type: Date,
-          },
-        notifications: {
-            type: Array,
-            default: null,
-          },
-    })
+var mongoose = require('mongoose');
+const SeekSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: [
+      /^(\+20|0)1[0-9]{9}$/,
+      'Please enter a valid Egyptian phone number',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  resetCode: { type: String, default: null },
+  resetCodeExpires: { type: Date, default: null },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['user'],
+  },
+  age: {
+    type: Number,
+    default: null,
+  },
+  city: {
+    type: String,
+    default: null,
+  },
+  region: {
+    type: String,
+    default: null,
+  },
+  accountDate: {
+    type: Date,
+  },
+  notifications: {
+    type: Array,
+    default: null,
+  },
+});
 
-    SeekSchema.methods.verifyPassword = async function (password) {
-      return bcrypt.compare(password, this.password);
-    };
-    
-    SeekSchema.pre("save", async function (next) {
-      if (!this.isModified("password")) return next();
-    
-      try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-      } catch (err) {
-        next(err);
-      }
-    });
- module.exports = mongoose.model('Seek', SeekSchema);
+SeekSchema.methods.verifyPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+SeekSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+module.exports = mongoose.model('Seek', SeekSchema);
