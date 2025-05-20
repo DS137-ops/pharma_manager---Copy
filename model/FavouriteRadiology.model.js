ddceec93dfb5a95cd6176d9ad57239a7b217cd72
fayad <feadkaffoura@gmail.com>
2025-05-20 21:44:59 +0300

    update ref

--- model/FavouriteRadiology.model.js
+++ model/FavouriteRadiology.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const favouriteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'seek', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Seek', required: true },
   specId: { type: mongoose.Schema.Types.ObjectId, ref: 'radiology', required: true },
   isFavourite: { type: Boolean, default: false },
 }, { timestamps: true });
