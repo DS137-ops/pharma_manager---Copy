ad66365cc0f7a2522d2fa45d84a6ab4f4db36e50
fayad <feadkaffoura@gmail.com>
2025-05-20 21:51:17 +0300

    up ana schema

--- model/FavouriteAnalyst.model.js
+++ model/FavouriteAnalyst.model.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
-
 const favouriteSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Seek', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'seek', required: true },
   specId: { type: mongoose.Schema.Types.ObjectId, ref: 'analyst', required: true },
   isFavourite: { type: Boolean, default: false },
 }, { timestamps: true });
