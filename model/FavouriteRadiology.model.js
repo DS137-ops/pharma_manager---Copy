512fcb518a161858bf467103bd6cb26ae2b943d2
fayad <feadkaffoura@gmail.com>
2025-05-20 13:45:04 +0300

    fix favo readiology

--- model/FavouriteRadiology.model.js
+++ model/FavouriteRadiology.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const favouriteSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'seek', required: true },
-  radiologyId: { type: mongoose.Schema.Types.ObjectId, ref: 'radiology', required: true },
+  specId: { type: mongoose.Schema.Types.ObjectId, ref: 'radiology', required: true },
   isFavourite: { type: Boolean, default: false },
 }, { timestamps: true });
 
