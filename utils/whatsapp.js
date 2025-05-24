c8d08d13dfcd60fad46181401aa6cb342cb6b010
hasan sarm <hasan.sarm.syria.h.s@gmail.com>
2025-05-24 23:44:54 +0300

    .

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -3,7 +3,22 @@ const qrcode = require('qrcode');
 
 let latestQr = ''; 
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        executablePath: '/usr/bin/chromium-browser', // مسار Chromium المثبت
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
+});
 
 client.initialize();
 
